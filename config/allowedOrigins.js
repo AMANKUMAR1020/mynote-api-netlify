cef936a4d1965a73ae9ccff8242a728c83109463
AMANKUMAR1020 <92974468+AMANKUMAR1020@users.noreply.github.com>
2023-08-28 09:49:12 +0530

    Update allowedOrigins.js
    https://cheerful-kangaroo-e24faa.netlify.app/ updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
 //    'https://mynote-uvp5.onrender.com'
-    'http://localhost:3000'
+//    'http://localhost:3000',
+    'https://cheerful-kangaroo-e24faa.netlify.app/',
 ]
 
 module.exports = allowedOrigins
